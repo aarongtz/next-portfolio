40b4f2813fb6a4be04becf1d8b95e7d7d3842b49
Aarón Gutiérrez <aarongtzdev@gmail.com>
2021-03-08 12:07:23 -0600

    Correccion de importacion de layout

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Link from 'next/link'
 
 export default function Home() {
